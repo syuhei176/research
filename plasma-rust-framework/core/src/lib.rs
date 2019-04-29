34cb47fd687d2fc13b7fee55afe72e8bf15eed8f
SHUHEI HIYA <syuhei176@gmail.com>
2019-04-29 22:45:42 +0900

    Add plasma-rust-framework

---
+++ plasma-rust-framework/core/src/lib.rs
@@ -0,0 +1,14 @@
+// Block
+#[derive(Clone)]
+pub struct Block {
+  number: u32
+}
+impl Block {
+  pub fn hello() {
+    println!("I am block");
+  }
+}
+
+pub fn main() {
+    println!("Hello!");
+}
---
+++ plasma-rust-framework/wallet-wasm/src/lib.rs
@@ -0,0 +1,35 @@
+mod utils;
+
+extern crate chamber_wallet;
+
+use self::chamber_wallet::{
+  Wallet,
+};
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    let wallet = Wallet{
+        number: 0
+    };
+    log("Welcome to wallet!")
+    log(&format!("The number is {}", wallet.hello()));
+}
